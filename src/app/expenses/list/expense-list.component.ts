09d641cbeb6fbf662d19a28cf5f9ad7d24cf7681
Yoeri Van Damme <Yoeri.VanDamme@sdworx.com>
2017-11-22 21:07:32 +0100

    manuele change detection

--- src/app/expenses/list/expense-list.component.ts
+++ src/app/expenses/list/expense-list.component.ts
@@ -1,13 +1,25 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy, ChangeDetectorRef, DoCheck } from '@angular/core';
 import { Expense } from '../expense'
 import { ExpenseService } from '../expenses.service'
 import { Observable } from 'rxjs/Observable'
+import { checkAndUpdateBinding } from '@angular/core/src/view/util';
 @Component({
     selector: 'expense-list',
-    templateUrl: 'expense-list.component.html'
+    templateUrl: 'expense-list.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class ExpenseListComponent {
+export class ExpenseListComponent implements DoCheck {
+    constructor(private changeDetection : ChangeDetectorRef){}
+    
+    previousListLength = 0;
+    ngDoCheck(): void {
+        const list = (this.list || []);
+        if(this.previousListLength !== list.filter(i => !!i['isHidden']).length){
+            this.previousListLength = this.list.length;
+            this.changeDetection.markForCheck();
+        }
+    }
 
     @Input() list: Expense[];
 
