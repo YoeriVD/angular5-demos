aae7eb9fbac1a14ddaeb92b1e3ec35fa6c9a583e
Yoeri Van Damme <Yoeri.VanDamme@sdworx.com>
2017-12-20 11:24:54 +0100

    trial with ContentChildren

--- src/app/grid/sortableColumn.directive.ts
+++ src/app/grid/sortableColumn.directive.ts
@@ -1,17 +1,28 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, OnInit, AfterContentInit,QueryList,ContentChildren } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 @Directive({ selector: 'table[sortable]', exportAs: 'column' })
-export class SortableColumnDirective implements OnInit {
+export class SortableColumnDirective implements AfterContentInit {
 
     constructor(private el: ElementRef) { }
     selectedColumn = 'id';
 
-    ngOnInit(): void {
-        const el: HTMLTableElement = this.el.nativeElement;
-        const elements = el.getElementsByTagName('th');
-        Array.prototype.forEach.call(elements, (element) => {
-            const th: HTMLTableHeaderCellElement = element;
-            th.addEventListener('click', () => this.selectedColumn = th.innerText.toLowerCase())
-        });
+    @ContentChildren('th', {descendants : true}) headers : QueryList<ElementRef>;
+
+    ngAfterContentInit(): void {
+        Observable
+        .from(this.headers.toArray())
+        .mergeMap(h => Observable.fromEvent(h.nativeElement, 'click'))
+        .map((ce : Event) => <HTMLTableHeaderCellElement>ce.target)
+        .map(th => th.innerText.toLowerCase())
+        .forEach(val => this.selectedColumn = val);
+
+
+        // const el: HTMLTableElement = this.el.nativeElement;
+        // const elements = el.getElementsByTagName('th');
+        // Array.prototype.forEach.call(elements, (element) => {
+        //     const th: HTMLTableHeaderCellElement = element;
+        //     th.addEventListener('click', () => this.selectedColumn = th.innerText.toLowerCase())
+        // });
     }
 }
